f1c6453ebbc520ed0d8037ebc1f3cf82f47755f5
Kye <promasterboy@outlook.com>
2022-06-27 21:27:56 +1000

    Add project files.

---
+++ API/MAL/MAL.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.Json;
+using Newtonsoft.Json;
+using System.Net.Http;
+namespace Anime3x3.API.MAL
+{
+    public static class MAL
+    {
+        //Using Jikan api wrapper
+        private const string BaseURL = "https://api.jikan.moe/v4";
+        public enum AnimeType
+        {
+            tv,
+            movie,
+            ova,
+            special,
+            ona,
+            music
+        }
+
+
+        public static async Task<SearchResponse.Rootobject> GetAnime(string search, bool sfw = true, params AnimeType[] types)
+        {
+            string sfwString = "";
+
+            if (sfw == true)
+            {
+                sfwString = "&sfw=true";
+            }
+
+            string? response = await Util.DownloadString(BaseURL + $"/anime?q={search}{TypesToQueryString(types)}{sfwString}&order_by=popularity&limit=1000");
+            JsonSerializerSettings settings = new();
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            if (response != null)
+                return JsonConvert.DeserializeObject<SearchResponse.Rootobject>(response, settings);
+            else
+                return null;
+        }
+
+
+        public static async Task<string> GetRandomAnimeName()
+        {
+            string S_resp = await Util.DownloadString(BaseURL + $"/random/anime");
+            if (!string.IsNullOrEmpty(S_resp))
+            {
+                var dataRoot = JsonDocument.Parse(S_resp).RootElement.GetProperty("data");
+
+                if (dataRoot.TryGetProperty("title_english", out JsonElement titleEN) && !string.IsNullOrEmpty(titleEN.ToString()))
+                {
+                    return titleEN.ToString();
+                }
+                else if (dataRoot.TryGetProperty("title", out JsonElement titleDefault) && !string.IsNullOrEmpty(titleDefault.ToString()))
+                {
+                    return titleDefault.ToString();
+                }
+                else if (dataRoot.TryGetProperty("title_japanese", out JsonElement titleJP) && !string.IsNullOrEmpty(titleJP.ToString()))
+                {
+                    return titleJP.ToString();
+                }
+            }
+
+
+            return null;
+        }
+
+        private static string TypesToQueryString(params AnimeType[] types)
+        {
+            string queryString = string.Empty;
+
+            foreach (var item in types)
+            {
+                queryString += $"&type={item}";
+
+            }
+
+            return queryString;
+        }
+
+
+    }
+}
---
+++ API/Util.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Anime3x3.API
+{
+    public static class Util
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        /// <summary>
+        /// Downloads a string, returns null if it's unsuccessful.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static async Task<string> DownloadString(string url) => await httpClient.GetStringAsync(url);
+    }
+}
